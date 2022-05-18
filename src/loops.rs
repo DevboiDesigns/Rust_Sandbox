3f1a35939d33a70e68fc5412ca093bc7bd537752
Christopher Hicks <visrule@protonmail.com>
2022-05-18 14:50:19 -0500

    loops

---
+++ src/loops.rs
@@ -0,0 +1,47 @@
+// Loops are use to iterate unitl a condition is meet
+
+
+pub fn run() {
+    let mut count = 0;
+
+    // Infinity loop
+    loop {
+        count += 1;
+        println!("Number {}", count);
+
+        if count == 20 {
+            break;
+        }
+    }
+
+
+    // While loop (FizzBuzz)
+    while count <= 100 {
+        if count % 15 == 0 {
+            println!("FizzBuzz");
+        } else if count % 3 == 0 {
+            println!("Fizz");
+        } else if count % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", count);
+        }
+
+        // Inc
+        count += 1
+    }
+
+    // For range
+    for x in 0..100 {
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
+
+}
